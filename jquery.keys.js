3cef9a976ba6dbc73f852e84a2b9cc65f4097805
Mike Dominice <mike@mikedominice.com>
2012-12-31 18:38:58 -0500

    first good swipe at plugin

--- jquery.keys.js
+++ jquery.keys.js
@@ -8,38 +8,89 @@
     }
 }(this, function ($) {
 
-    var defaults = {
-        noteOnVelocity : 0x7f, // 128
-        noteOffVelocity : 0x40 // 64
-    };
-
-    var NOTE_ON = 0x90;
-    var NOTE_OFF = 0x80;
-
-    var pluginName = 'midiKeys';
-    var pluginDataName = 'plugin_' + pluginName;
-
-    var keys = 'ZSXSCVGBHNJM' + 'Q2W3ER5T6Y7U' + 'I9O0P';
-
-    var startNote = 48; // C3
-
-    var events = {};
-    var eventPrefix = 'event_';
-
-    var eventKey = function (charCode) {
-        return eventPrefix + charCode;
-    };
-
-    var createMIDIEvent = function (event, data) {
-        return {
-            timestamp : event.timeStamp,
-            data : data
+    var NOTE_ON = 0x90,
+        NOTE_OFF = 0x80,
+        notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
+        defaults = {
+            startNote : 48, // C3
+            noteOnVelocity : 0x7f, // 128
+            noteOffVelocity : 0x40, // 64,
+            channel : 0
+        },
+        pluginName = 'midiKeys',
+        pluginDataName = 'plugin_' + pluginName,
+        keys = 'ZSXDCVGBHNJM' + 'Q2W3ER5T6Y7U' + 'I9O0P',
+        createMIDIEventData = function (status, note, velocity) {
+            var data = new Uint8Array(3);
+            data[0] = status;
+            data[1] = note;
+            data[2] = velocity;
+            return data;
+        },
+        getEventCode = function (event) {
+            var character = String.fromCharCode(event.keyCode),
+                index = keys.indexOf(character);
+            if (index < 0) {
+                return;
+            }
+            return this.options.startNote + index;
+        },
+        createEventCallback = function (status, velocityKey) {
+            return function (event) {
+                var eventCode = getEventCode.call(this, event);
+                if (!eventCode) {
+                    return;
+                }
+                var velocity = this.options[velocityKey];
+                if ($.isFunction(velocity)) {
+                    velocity = velocity();
+                }
+                var data = createMIDIEventData(status | this.options.channel, eventCode, velocity);
+                var midiEvent = new MIDIEvent(event.timeStamp, data);
+                this.$element.trigger('midiEvent', midiEvent);
+            };
         };
-    };
 
-    $.each(keys, function (i, key) {
-        events[eventKey(key.charCodeAt(0))] = startNote + i;
-    });
+    function MIDIEvent(timestamp, data) {
+        this.timestamp = timestamp;
+        this.data = data;
+    }
+
+    MIDIEvent.prototype = {
+        getChannel : function () {
+            return this.data[0] & 0x0f;
+        },
+        getVelocity : function () {
+            return this.data[2];
+        },
+        getOctave : function () {
+            return Math.floor(this.data[1] / 12) - 1;
+        },
+        getNoteLetter : function () {
+            return notes[this.data[1] % 12];
+        },
+        getNote : function () {
+            return this.getNoteLetter() + this.getOctave();
+        },
+        toString : function () {
+            var parts = [
+                'type=' + this.getType(),
+                'channel=' + this.getChannel(),
+                'note=' + this.getNote(),
+                'velocity=' + this.getVelocity()
+            ];
+            return 'MIDIEvent[' + parts.join(',') + ']';
+        },
+        getType : function () {
+            var statusWithoutChannel = this.data[0] & 0xf0;
+            if (statusWithoutChannel === NOTE_ON) {
+                return 'NOTE ON';
+            }
+            if (statusWithoutChannel === NOTE_OFF) {
+                return 'NOTE OFF';
+            }
+        }
+    };
 
     function MIDIKeys (element, options) {
         this.element = element;
@@ -54,42 +105,25 @@
 
     MIDIKeys.prototype = {
         init : function () {
-            var keydown = $.proxy(this.keydown, this);
-            var keyup = $.proxy(this.keyup, this);
+            var keydown = $.proxy(createEventCallback(NOTE_ON, 'noteOnVelocity'), this),
+                keyup = $.proxy(createEventCallback(NOTE_OFF, 'noteOffVelocity'), this);
             this.$element.on('keydown', keydown);
             this.$element.on('keyup', keyup);
             this.destroy = function () {
                 this.$element.off('keydown', keydown);
                 this.$element.off('keyup', keyup);
+                this.$element.removeData(pluginDataName);
             };
-        },
-        keydown : function (event) {
-            var eventCode = events[eventKey(event.keyCode)];
-            if (!eventCode) {
-                return;
-            }
-            var data = new Uint8Array(3);
-            data[0] = NOTE_ON;
-            data[1] = eventCode;
-            data[2] = this.options.noteOnVelocity;
-            this.$element.trigger('midiEvent', createMIDIEvent(event, data));
-        },
-        keyup : function (event) {
-            var eventCode = events[eventKey(event.keyCode)];
-            if (!eventCode) {
-                return;
-            }
-            var data = new Uint8Array(3);
-            data[0] = NOTE_ON;
-            data[1] = eventCode;
-            data[2] = this.options.noteOnVelocity;
-            this.$element.trigger('midiEvent', createMIDIEvent(event, data));
         }
     };
 
     return $.fn[pluginName] = function (options) {
+        var args = Array.prototype.slice.call(arguments, 1);
         return this.each(function () {
-            if (!$.data(this, pluginDataName)) {
+            var pluginData = $.data(this, pluginDataName);
+            if (pluginData && $.isFunction(pluginData[options])) {
+                pluginData[options].apply(pluginData, args);
+            } else if (!pluginData) {
                 $.data(this, pluginDataName, new MIDIKeys(this, options));
             }
         });
