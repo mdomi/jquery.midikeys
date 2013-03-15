5318bd7c7a76e09eca6b2d83edbb5aa2a96c6c5a
Mike Dominice <mike@mikedominice.com>
2013-03-15 18:27:42 -0400

    added first functional tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-(function () {
+(function ($) {
     'use strict';
 
     test('plugin is set up', function () {
@@ -7,4 +7,30 @@
 
     });
 
-}());
+    test('plugin initializes with defaults without error, returning the element', function () {
+
+        var $el = $('#test-target');
+
+        equal($el, $el.midiKeys());
+
+    });
+
+    asyncTest('translates keydown events into MIDI NOTE ON messages', function () {
+        expect(3);
+
+        var $el = $('#test-target').midiKeys();
+
+        $el.on('message', function (event, message) {
+            console.log(event);
+            equal(message.data[0], 0x90, 'Should be a MIDI NOTE ON event');
+            equal(message.data[1], 48, 'Should be a C3 note');
+            equal(message.data[2], 127, 'Should have default velocity');
+            start();
+        });
+
+        var event = $.Event('keydown');
+        event.keyCode = 'Z'.charCodeAt(0);
+        $el.trigger(event);
+    });
+
+}(jQuery));
