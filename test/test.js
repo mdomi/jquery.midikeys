dbcd8d515be9b5f867eda20199f30ffa68e5d517
Michael Dominice <michael.dominice@issinc.com>
2014-05-13 09:23:25 -0400

    fixed unit tests, refactored code

--- test/test.js
+++ test/test.js
@@ -1,36 +1,44 @@
 (function (document, $) {
     'use strict';
 
+    module('setup');
     test('plugin is set up', function () {
-
         ok($.fn.hasOwnProperty('midiKeys'), 'plugin should be attached to jQuery prototype');
-
     });
 
+    var $el;
+
+    module('plugin', {
+        teardown : function () {
+            if ($el) {
+                $el.midiKeys('destroy');
+            }
+        }
+
+    });
     test('plugin initializes with defaults without error, returning the element', function () {
-
-        var $el = $('#test-target');
-
+        $el = $('#test-target');
         equal($el, $el.midiKeys());
 
     });
 
-    asyncTest('translates keydown events into MIDI NOTE ON messages', function () {
-        expect(3);
+    test('translates keydown events into MIDI NOTE ON messages', function () {
+        var receivedMessage;
 
-        var $el = $('#test-target').midiKeys();
-
-        $el.on('message', function (event, message) {
-            equal(message.data[0], 0x90, 'Should be a MIDI NOTE ON event');
-            equal(message.data[1], 48, 'Should be a C3 note');
-            equal(message.data[2], 127, 'Should have default velocity');
-            start();
+        $el = $('#test-target').midiKeys({
+            onmidimessage : function (message) {
+                receivedMessage = message;
+            }
         });
 
         var event = document.createEvent('HTMLEvents');
         event.initEvent('keydown', true, false);
         event.keyCode = 'Z'.charCodeAt(0);
         $el[0].dispatchEvent(event);
+
+        equal(receivedMessage.data[0], 0x90, 'Should be a MIDI NOTE ON event');
+        equal(receivedMessage.data[1], 48, 'Should be a C3 note');
+        equal(receivedMessage.data[2], 127, 'Should have default velocity');
     });
 
 }(this.document, this.jQuery));
