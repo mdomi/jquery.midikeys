6db6c28ac25092ae210c462d5898ef4e0e299e16
Mike Dominice <mike@mikedominice.com>
2014-06-27 17:37:18 -0400

    finished adding karma tests

--- test/test.js
+++ test/test.js
@@ -1,44 +1,58 @@
+/* global describe: false, it: false, beforeEach: false, afterEach: false */
 (function (document, $) {
     'use strict';
 
-    module('setup');
-    test('plugin is set up', function () {
-        ok($.fn.hasOwnProperty('midiKeys'), 'plugin should be attached to jQuery prototype');
-    });
+    describe('jquery.midiKeys', function () {
 
-    var $el;
+        describe('plugin registration', function () {
 
-    module('plugin', {
-        teardown : function () {
-            if ($el) {
-                $el.midiKeys('destroy');
-            }
-        }
+            it('should be attached to jQuery prototype', function () {
+                expect($.fn.hasOwnProperty('midiKeys')).to.equal(true);
+            });
 
-    });
-    test('plugin initializes with defaults without error, returning the element', function () {
-        $el = $('#test-target');
-        equal($el, $el.midiKeys());
+            it('initializes with defaults withour error, returning the element', function () {
+                var $el = $('<div></div>');
+                expect($el).to.equal($el.midiKeys());
+            });
 
-    });
-
-    test('translates keydown events into MIDI NOTE ON messages', function () {
-        var receivedMessage;
-
-        $el = $('#test-target').midiKeys({
-            onmidimessage : function (message) {
-                receivedMessage = message;
-            }
         });
 
-        var event = document.createEvent('HTMLEvents');
-        event.initEvent('keydown', true, false);
-        event.keyCode = 'Z'.charCodeAt(0);
-        $el[0].dispatchEvent(event);
+        describe('execution', function () {
 
-        equal(receivedMessage.data[0], 0x90, 'Should be a MIDI NOTE ON event');
-        equal(receivedMessage.data[1], 48, 'Should be a C3 note');
-        equal(receivedMessage.data[2], 127, 'Should have default velocity');
+            function createKeyEvent(c) {
+                var event = document.createEvent('HTMLEvents');
+                event.initEvent('keydown', true, false);
+                event.keyCode = c.charCodeAt(0);
+                return event;
+            }
+
+            var $el;
+
+            beforeEach(function () {
+                $el = $('<div></div>').midiKeys();
+                $el.appendTo(document.body);
+            });
+
+            it('translates keydown events into MIDI NOTE ON messages', function (done) {
+                $el.midiKeys('option', 'onmidimessage', function (message) {
+                    expect(message.data[0]).to.equal(0x90, 'Should be a MIDI NOTE ON event');
+                    expect(message.data[1]).to.equal(48, 'Should be a C3 note');
+                    expect(message.data[2]).to.equal(127, 'Should have default velocity');
+                    done();
+                });
+
+                $el[0].dispatchEvent(createKeyEvent('Z'));
+            });
+
+            afterEach(function () {
+                $el.midiKeys('destroy');
+                $el.remove();
+            });
+        });
+
+
+
+
     });
 
 }(this.document, this.jQuery));
