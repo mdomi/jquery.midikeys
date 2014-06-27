314c9d7a2432e4d7fd6f623a8cb4b3b0057aac6b
Mike Dominice <mike@mikedominice.com>
2014-06-27 18:07:02 -0400

    added tests for non-jQuery bound usage

--- test/test.js
+++ test/test.js
@@ -1,6 +1,36 @@
 /* global describe: false, it: false, beforeEach: false, afterEach: false */
-(function (document, $) {
+(function (document, $, MIDIKeys) {
     'use strict';
+
+    function createKeyEvent(c) {
+        var event = document.createEvent('HTMLEvents');
+        event.initEvent('keydown', true, false);
+        event.keyCode = c.charCodeAt(0);
+        return event;
+    }
+
+    describe('MIDIKeys', function () {
+
+        var el;
+
+        beforeEach(function () {
+            el = document.createElement('div');
+        });
+
+        it('translates keydown events into MIDI NOTE ON messages', function (done) {
+            var plugin = new MIDIKeys(el);
+
+            plugin.option('onmidimessage', function (message) {
+                expect(message.data[0]).to.equal(0x90, 'Should be a MIDI NOTE ON event');
+                expect(message.data[1]).to.equal(48, 'Should be a C3 note');
+                expect(message.data[2]).to.equal(127, 'Should have default velocity');
+                done();
+            });
+
+            el.dispatchEvent(createKeyEvent('Z'));
+        });
+
+    });
 
     describe('jquery.midiKeys', function () {
 
@@ -19,18 +49,10 @@
 
         describe('execution', function () {
 
-            function createKeyEvent(c) {
-                var event = document.createEvent('HTMLEvents');
-                event.initEvent('keydown', true, false);
-                event.keyCode = c.charCodeAt(0);
-                return event;
-            }
-
             var $el;
 
             beforeEach(function () {
                 $el = $('<div></div>').midiKeys();
-                $el.appendTo(document.body);
             });
 
             it('translates keydown events into MIDI NOTE ON messages', function (done) {
@@ -46,13 +68,9 @@
 
             afterEach(function () {
                 $el.midiKeys('destroy');
-                $el.remove();
             });
         });
 
-
-
-
     });
 
-}(this.document, this.jQuery));
+}(this.document, this.jQuery, this.MIDIKeys));
